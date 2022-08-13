6ff03056fa4df55aa30093afa93f002b9f29d2cb
Paul Haubourdin <paul.haubourdin@gmail.com>
2022-08-13 23:46:32 +0700

    renamed some items for clarity and commented out code used for comparing airac cycles

--- main.py
+++ main.py
@@ -23,11 +23,6 @@ print("Reading subscriber emails")
 with open(paths.subscribers, 'r') as file:
     subscribers = file.read().splitlines()
 
-# Move previously generated summary files to archive folders
-print("Moving last cycle's files to archive folders")
-paths.archiveFiles(paths.path_dict['listing_current'], paths.path_dict['listing_archive'])
-paths.archiveFiles(paths.path_dict['pdf_current'], paths.path_dict['pdf_archive'])
-
 for airport in airports:
     # Get current DAP files for each of the aerodromes
     print(f"Getting latest DAP files for {airport}")
@@ -35,23 +30,27 @@ for airport in airports:
     # Get current ERSA files for each of the aerodromes
     print(f"Getting latest ERSA files for {airport}")
     ersa_new = update.ERSA(airport, airac, paths)
-    # Get file listing for previous cycle
-    print(f"Reading previous file listings for {airport}")
-    csv_dap_previous = paths.getLatestFile('DAP', airport, paths.path_dict['listing_archive'])
-    csv_ersa_previous = paths.getLatestFile('ERSA', airport, paths.path_dict['listing_archive'])
-    # Get file listing for current cycle
-    print(f"Reading current file listings for {airport}")
-    csv_dap_current = paths.getLatestFile('DAP', airport, paths.path_dict['listing_current'])
-    csv_ersa_current = paths.getLatestFile('ERSA', airport, paths.path_dict['listing_current'])
+    # Following code is meant to identify changes and notify subscribers of changes via email
+    # It is commented out because email sending needs to be updated for gmail security issues
+    # It is also considered faulty because archive will include files from cycles prior to previous cycle
+    # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    # # Get file listing for previous cycle
+    # print(f"Reading previous file listings for {airport}")
+    # csv_dap_previous = paths.getLatestFile('DAP', airport, paths.path_dict['file_list_archive'])
+    # csv_ersa_previous = paths.getLatestFile('ERSA', airport, paths.path_dict['file_list_archive'])
+    # # Get file listing for current cycle
+    # print(f"Reading current file listings for {airport}")
+    # csv_dap_current = paths.getLatestFile('DAP', airport, paths.path_dict['file_list_current'])
+    # csv_ersa_current = paths.getLatestFile('ERSA', airport, paths.path_dict['file_list_current'])
 
-    # Runs comparison of link data and filenames of previously downloaded files against new files
-    print(f"Comparing file listings for {airport}")
-    compare_dap = compare_notify.Comparison(
-        'DAP',
-        airport,
-        csv_dap_previous,
-        csv_dap_current,
-        paths.path_dict['report_archive'],
-        subscribers)
+    # # Runs comparison of link data and filenames of previously downloaded files against new files
+    # print(f"Comparing file listings for {airport}")
+    # compare_dap = compare_notify.Comparison(
+    #     'DAP',
+    #     airport,
+    #     csv_dap_previous,
+    #     csv_dap_current,
+    #     paths.path_dict['report_archive'],
+    #     subscribers)
     # # Runs comparison of content of previously downloaded files against new files
     # compare_ersa = compare_notify.Comparison('ERSA', airport, csv_ersa_previous, csv_ersa_current)
--- paths.py
+++ paths.py
@@ -9,8 +9,8 @@ class Paths:
         self.subscribers = os.path.join(self.root_path, "subscribers.csv")
         self.path_names = [
             "html_archive",
-            "listing_archive",
-            "listing_current",
+            "file_list_archive",
+            "file_list_current",
             "pdf_archive",
             "pdf_current",
             "pdf_archive_raw",
@@ -18,30 +18,37 @@ class Paths:
         ]
         self.path_dict = {}
         for path in self.path_names:
-            path_data = self.addPath(path)
+            path_data = self.init_addPath(path)
             self.path_dict[path_data[0]] = path_data[1]
+        print("Moving last cycle's files to archive folders")
+        self.init_archiveFiles(self.path_dict['file_list_current'], self.path_dict['file_list_archive'])
+        self.init_archiveFiles(self.path_dict['pdf_current'], self.path_dict['pdf_archive'])
 
-    def addPath(self, path_name):
+    def init_addPath(self, path_name):
         """ Checks to see if required directory exists, creates it if it doesn't exist """
         full_path = os.path.join(self.root_path, path_name)
         if not os.path.isdir(full_path):
             os.mkdir(full_path)
         return [path_name, full_path]
 
-    def archiveFiles(self, current_path, archive_path):
+    def init_archiveFiles(self, current_path, archive_path):
         """ Move files from current folder to an archive folder """
         for file_name in os.listdir(current_path):
             current = os.path.join(current_path, file_name)
             archive = os.path.join(archive_path, file_name)
             os.replace(current, archive)
 
-    def getLatestFile(self, type, airport, path):
-        file_list = [file for file in os.listdir(path) if type in file if airport in file]
-        if file_list == []:
-            print("No relevant file(s) in archive.")
-            return "na"
-        else:
-            file_list.sort(reverse=True)
-            file_path = os.path.join(path, file_list[0])
-            return file_path
+    # Code below possibly requires updated to ensure only files from previous airac cycle are listed
+    # This is needed because comparison should only list changes from previous cycle
+    # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    # def getLatestFile(self, type, airport, path):
+    #     """ Used in main.py to compare previous files against current files """
+    #     file_list = [file for file in os.listdir(path) if type in file if airport in file]
+    #     if file_list == []:
+    #         print("No relevant file(s) in archive.")
+    #         return "na"
+    #     else:
+    #         file_list.sort(reverse=True)
+    #         file_path = os.path.join(path, file_list[0])
+    #         return file_path
      
